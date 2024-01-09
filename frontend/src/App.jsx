ff42ae8f5998755f2f0396749890313ab33fe33a
Brendon Chow <brendonchow123@gmail.com>
2024-01-09 22:50:16 +1100

    Add Docker

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,17 @@ import axios from "axios";
 function App() {
   const [message, setMessage] = useState("");
   useEffect(() => {
-    axios.get("http://localhost:3000/").then((result) => {
-      setMessage(result.data);
+    axios.get("http://localhost:3000/").then((res) => {
+      setMessage(res.data);
     });
-  });
+  }, []);
 
-  return <h1>{message}</h1>;
+  return (
+    <>
+      <h1>Front End working</h1>
+      <h1>{message}</h1>
+    </>
+  );
 }
 
 export default App;
