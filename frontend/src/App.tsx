9059249cbdfafab3211ecf6c713fb519c97682b6
Brendon Chow <brendonchow123@gmail.com>
2024-01-13 20:34:50 +1100

    Add typescript to the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const fetchMessage = async () => {
+      const response = await fetch("http://localhost:3000/");
+      const data = await response.json();
+      setMessage(data);
+    };
+    fetchMessage();
+  }, []);
+
+  return (
+    <>
+      <h1>Front end working</h1>
+      <h1>{message}</h1>
+    </>
+  );
+}
+
+export default App;
